e07ef7511c4b9eb11711284493ba6fa2ae54458d
Damon <damonsorensen@gmail.com>
2020-03-08 00:34:07 -0700

    Generic Agent Methods

---
+++ Modules/ZendeskAgentBot.py
@@ -0,0 +1,75 @@
+import requests
+import json
+
+
+class ZendeskAgentBot:
+    """Class that represents a Zendesk Agent, can Create, Update, and Get all Zendesk Tickets"""
+
+    baseAPI = ''
+    newTickets = []
+    openTickets = []
+    solvedTickets = []
+    username = ''
+    password = ''
+
+# Setting the initial values of username, password, and baseAPI
+    def __init__(self, username, password, baseAPI):
+        self.username = username
+        self.password = password
+        self.baseAPI = baseAPI
+
+    def getNewTickets(self):
+        """Gets all NEW Zendesk Tickets, sets them to an array in ZendeskAgentBot.newTickets"""
+
+        request = requests.get(self.baseAPI + 'search.json?query=type:ticket status:new', auth=(self.username, self.password))
+        response = request.text
+        tickets = json.loads(response)['tickets'] if json.loads(
+            response)['count'] != 0 else None
+        if tickets != None:
+            for ticket in tickets:
+                if ticket is None:
+                    continue
+                else:
+                    self.newTickets.append(ticket)
+                pass
+        else:
+            print('No New Tickets')
+
+    def getOpenTickets(self):
+        """Gets all OPEN Zendesk Tickets, sets them to an array in ZendeskAgentBot.openTickets"""
+
+        request = requests.get(self.baseAPI + 'search.json?query=type:ticket status:open', auth=(self.username, self.password))
+        response = request.text
+        tickets = json.loads(response)['results'] if json.loads(
+            response)['count'] != 0 else None
+        if tickets != None:
+            for ticket in tickets:
+                if ticket is None:
+                    continue
+                else:
+                    self.openTickets.append(ticket)
+                pass
+        else:
+            print('No Open Tickets')
+
+    def getSolvedTickets(self):
+        """Gets all SOLVED Zendesk Tickets, sets them to an array in ZendeskAgentBot.solvedTickets"""
+
+        request = requests.get(self.baseAPI + 'search.json?query=type:ticket status:open', auth=(self.username, self.password))
+        response = request.text
+        tickets = json.loads(response)['results'] if json.loads(
+            response)['count'] != 0 else None
+        if tickets != None:
+            for ticket in tickets:
+                if ticket is None:
+                    continue
+                else:
+                    self.solvedTickets.append(ticket)
+                pass
+        else:
+            print('No Open Tickets')
+
+    def replyToTicket(self, ticketId):
+        """Gets and comments on a ticket, specified by ticket id"""
+
+        request = requests.put(self.baseAPI)
